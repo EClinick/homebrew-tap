6b273320bdba0b48147c1e6947408c356bf4a511
Ethan Clinick <Ethan@Clinick.net>
2026-07-10 09:24:47 -0700

    Update Juice to 0.1.0

--- Casks/juice.rb
+++ Casks/juice.rb
@@ -1,11 +1,13 @@
 cask "juice" do
-  version "0.1.0-dev.2"
-  sha256 "ca87f8ccebd9b073af24ab16a8f13f21110007b2d8abc693c91b888e4c5af0b3"
+  version "0.1.0"
+  sha256 "88f0f06d07a53e67e87ddac605b235b915c21212fe66a28ca14daf5453058aad"
 
   url "https://github.com/EClinick/juice/releases/download/v#{version}/Juice.dmg"
   name "Juice"
   desc "Menu bar battery energy monitor"
   homepage "https://github.com/EClinick/juice"
+
+  depends_on macos: :sonoma
 
   app "Juice.app"
 
@@ -15,7 +17,6 @@ cask "juice" do
   ]
 
   caveats <<~EOS
-    Juice #{version} is an ad-hoc-signed development build and is not notarized.
     Per-app energy history also requires Juice's separately installed privileged helper.
   EOS
 end
