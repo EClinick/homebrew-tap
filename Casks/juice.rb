fca162fdbbe96d3da5e770b710c426fa6319904d
Ethan Clinick <Ethan@Clinick.net>
2026-07-22 21:18:50 -0700

    Update Juice to 0.2.2

--- Casks/juice.rb
+++ Casks/juice.rb
@@ -1,6 +1,6 @@
 cask "juice" do
-  version "0.2.1"
-  sha256 "24da5612a95a2ed4f62464c718a74e8452f5d06a7cc1a385fb64b4d7bf19e056"
+  version "0.2.2"
+  sha256 "ae573fe7729c4f7055072622993cd59e1d62b5332e23df71d28580892e87d535"
 
   url "https://github.com/EClinick/juice/releases/download/v#{version}/Juice.dmg"
   name "Juice"
